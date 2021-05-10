ca6aa6d038a85d36c7052bbaba53156887a686d6
pradyumn <allcodingwebsites@gmail.com>
2021-05-10 14:57:01 +0530

    sending patient photo and exercise video url to relative

--- src/controllers/Relative.js
+++ src/controllers/Relative.js
@@ -12,7 +12,7 @@ const Relative = {
         const cur_date = moment(new Date()).format(date_format);
         const myNumber = 'SELECT * FROM users WHERE user_id = $1';
         const accepted = "A";
-        const query = 'SELECT profile_page.first_name, profile_page.last_name, date_info.today_day, exercises.exercise_name, profile_page.user_id, date_info.exercise_id, date_info.marked_by_relative FROM ((((date_info INNER JOIN treatment ON treatment.treatment_id = date_info.treatment_id AND date_info.marked_by_patient = 1 AND treatment.treatment_day > 0) INNER JOIN profile_page ON profile_page.mobile_number = treatment.patient_number) INNER JOIN patient ON patient.user_id = profile_page.user_id AND ((patient.relative_1 = ($1) AND patient.relative_1_status = ($3)) OR (patient.relative_2 = ($2) AND patient.relative_2_status = ($3)))) INNER JOIN exercises ON exercises.exercise_id = date_info.exercise_id)';
+        const query = 'SELECT profile_page.first_name, profile_page.last_name,profile_page.profile_pic, date_info.today_day, exercises.exercise_name, exercises.exercise_video_url, profile_page.user_id, date_info.exercise_id, date_info.marked_by_relative FROM ((((date_info INNER JOIN treatment ON treatment.treatment_id = date_info.treatment_id AND date_info.marked_by_patient = 1 AND treatment.treatment_day > 0) INNER JOIN profile_page ON profile_page.mobile_number = treatment.patient_number) INNER JOIN patient ON patient.user_id = profile_page.user_id AND ((patient.relative_1 = ($1) AND patient.relative_1_status = ($3)) OR (patient.relative_2 = ($2) AND patient.relative_2_status = ($3)))) INNER JOIN exercises ON exercises.exercise_id = date_info.exercise_id)';
         try{
             const user = await db.query(myNumber, [myID]);
             if (!user.rows[0]) {
